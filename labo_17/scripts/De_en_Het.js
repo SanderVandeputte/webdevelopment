8cac6dd92b291ae1fee2203f772e08e46e769e04
SanderVandeputte <sander.vandeputte@student.vives.be>
2026-03-16 10:25:31 +0100

    fix

--- labo_17/scripts/De_en_Het.js
+++ labo_17/scripts/De_en_Het.js
@@ -1,11 +1,11 @@
 const setup = () => {
-    const zin = "Gisteren zat de jongen op de stoep en at de helft van de appel";
+    const zin = "De man rief de";
     let resultaat = "";
     let output = document.getElementById("zin");
 
 
     for (let i = 0; i < zin.length; i++) {
-        if (zin.substring(i, i + 2) === "de" && (i === 0 || zin[i-1] === " ") && (i + 2 === zin.length || zin[i+2] === " ")) {
+        if (zin.substring(i, i + 2) === "de" ||  zin.substring(i, i + 2) === "De" && (i === 0 || zin[i-1] === " ") && (i + 2 === zin.length || zin[i+2] === " ")) {
             resultaat += "het";
             i++;
         } else {
