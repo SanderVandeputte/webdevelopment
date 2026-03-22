55953bc28a4d77cbcda3e66dae2e905a36a285f5
SanderVandeputte <sander.vandeputte@student.vives.be>
2026-03-22 22:36:12 +0100

    labo 18 voorlopig

---
+++ labo_18/scripts/oefening_nodes_2.js
@@ -0,0 +1,25 @@
+const setup = () => {
+    const listItem = document.querySelectorAll('li');
+    listItem.forEach(item => {
+        item.setAttribute("class", "listitem");
+    })
+
+
+
+    const style = document.createElement("style")
+    style.innerText = ".listitem {color: red;}";
+    document.head.appendChild(style);
+
+    const image = document.createElement("img");
+    image.setAttribute("src", "../images/image_nodes_2.png");
+    image.setAttribute("alt", "een afbeelding");
+    document.body.appendChild(image);
+}
+
+
+
+
+
+
+
+window.addEventListener('load', setup)
